c7fd961496cf43925ce76bfca6edccf728d18aad
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-02-11 14:50:14 +0530

    updated

---
+++ my-app/src/myComponents/Square.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Square = ({value}:any) => {
+    console.log(value)
+  return (
+    
+    <button>
+        {value}
+    </button>
+  )
+}
+
+export default Square
